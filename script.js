e53dfe80d817f56454c02b8d44f7d44cc8d39c08
joao17-bel <jgbarbosa721@gmail.com>
2024-09-05 20:48:55 -0300

    testes

---
+++ script.js
@@ -0,0 +1,30 @@
+//var fala1 ="nao sou eu"
+//var fala2 ="eu tentei te ajudar desde o inicio" 
+//var fala3 ="Já pensou que o montro pode ser comtrolado"
+
+//var conta1 = fala1.length;
+//var conta2 = fala2.length;
+//var conta3 = fala3.length;
+
+//if(conta1>conta2 && conta1>conta3){
+    //console.log(`o impostor é o numero 1`)
+ //}else if(conta2>conta3 && conta2>conta1){
+   // console.log(`o impostor é o numero 2`)
+ //}else{
+   // console.log(`o impostor é o numero 3`)
+ //}
+
+ var numero1 = 47;
+ var numero2 = 86;
+ var numero3 = 39;
+
+if(numero1>numero2 && numero3>numero2){
+    numero2 = numero3+numero1
+    console.log(`${numero2}`)
+}else if(numero1-numero2+numero3>=100){
+    var soma =numero1+numero2+numero3;
+    console.log(`${soma}`)
+}else{
+    var mult =numero1*numero2*numero3
+    console.log(`${mult}`)
+}
